59aa578e8d8eeaadbb12620d981cce57e348eeea
Jared Wasinger <j-wasinger@hotmail.com>
2016-03-12 17:51:25 -0500

    add chunk header format

---
+++ common/chunk_header.h
@@ -0,0 +1,10 @@
+﻿#ifndef CLIENT_SERVER_COMMMON
+#define CLIENT_SERVER_COMMMON
+
+//total size should be 64 bytes (probably subject to change)
+struct MessageHeader {
+  char file_name[30];
+  int file_size;
+}
+
+#endif
